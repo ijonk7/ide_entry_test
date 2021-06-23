ad8785d19bf30f19242e9b9fe7c9c9733c7eda36
ijonk7 <jookeo.rizal@gmail.com>
2021-06-24 03:27:06 +0700

    Pull Request Test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pull-request-test', function () {
+    return 'Pull Request Test';
+});
